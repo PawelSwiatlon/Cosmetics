04649f09aeed13b80363e185ae62e9f9a8651003
PawelSwiatlon <pswiatlon02@gmail.com>
2021-02-15 19:38:23 +0100

    test

--- src/components/Heding/Heding.js
+++ src/components/Heding/Heding.js
@@ -4,7 +4,6 @@ import styles from './Heading.module.scss';
 const Heding = () => {
     return(
         <div className={styles.wrapper}>
-            <div className={styles.blur}></div>
         </div>
     )
 }
